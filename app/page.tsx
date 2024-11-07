a0efc1ba79fbc638ef7c04cfa8042cb4805ca358
nazrul <108456207+NazrulIslam-Chowdhury@users.noreply.github.com>
2024-11-07 12:31:45 +0600

    model component created

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,13 @@
 
 import Hero from "@/components/Hero";
 import Highlights from "@/components/Highlights";
+import Model from "@/components/Model";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <main>
-      <Navbar /> <Hero /> <Highlights />
+      <Navbar /> <Hero /> <Highlights /> <Model />
     </main>
   );
 }
